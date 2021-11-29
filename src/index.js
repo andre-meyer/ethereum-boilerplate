51f40f318fbe2058217854c4567ad4a3f3aa9ab0
Andre Meyer <andre.meyer@adesso.de>
2021-11-29 15:17:48 +0100

    boilerplate init

---
+++ src/index.js
@@ -0,0 +1,14 @@
+import React from "react";
+import CssBaseline from '@mui/material/CssBaseline';
+
+import { render } from "react-dom";
+import App from "./components/App";
+
+const root = document.getElementById("root");
+
+render(
+  <React.Fragment>
+    <CssBaseline />
+    <App />
+  </React.Fragment>
+, root);
---
+++ webpack.config.js
@@ -0,0 +1,39 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+const isDev = process.env.NODE_ENV === "development";
+
+module.exports = {
+  mode: isDev ? "development" : "production",
+  entry: path.resolve("./src/index.js"),
+  devtool: isDev ? 'eval-cheap-module-source-map' : false,
+  module: {
+    rules: [
+      {
+        test: /\.m?jsx?$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader",
+          options: {
+            presets: ['@babel/preset-env']
+          }
+        }
+      }
+    ]
+  },
+  resolve: {
+    extensions: ['*', '.js', '.jsx'],
+  },
+  devServer: {
+    port: 8080,
+    hot: true,
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: 'src/html/index.html'
+    })
+  ],
+  performance: {
+    hints: false
+  },
+};
