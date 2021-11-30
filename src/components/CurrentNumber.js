e1690b5fbdfa1e7b782dc646dbec9670454e4d91
Andre Meyer <andre.meyer@adesso.de>
2021-11-30 14:41:42 +0100

    current number load

--- src/components/CurrentNumber.js
+++ src/components/CurrentNumber.js
@@ -1,16 +1,16 @@
 import React, { useEffect, useState } from "react";
 import { Container, Typography } from "@mui/material";
 import { Box } from "@mui/system";
+import { getNumber } from "../api"
 
 import MyContract from "../ethereum/contract/MyContract";
 
 const CurrentNumber = () => {
-  const [currentNumber, setCurrentNumber] = useState('');
+  const [currentNumber, setCurrentNumber] = useState('?');
 
   useEffect(() => {
     (async () => {
-      const instance = await MyContract.deployed();
-      const num = (await instance.getNumber()).toNumber();
+      const num = await getNumber();
       setCurrentNumber(num);
     })();
   }, [])
